d0c08ca31894bdb61e4daa299d0f3f54e98fd1b2
amitkshirsagar13 <amit.kshirsagar.13@gmail.com>
2020-02-09 21:15:27 +0530

    api call working

--- src/app/core/components/user/user.component.ts
+++ src/app/core/components/user/user.component.ts
@@ -3,6 +3,8 @@ import { User, UsersService, Configuration} from '@amitkshirsagar13/user-auth-se
 import { MatTableDataSource } from '@angular/material';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../../service/auth.service';
+import { UserService } from '../../service/user.service';
+import { stringify } from 'querystring';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -11,20 +13,19 @@ import { AuthService } from '../../service/auth.service';
 export class UserComponent implements OnInit {
   displayedColumns: string[] = ['id', 'userName', 'firstName', 'lastName'];
   dataSource: MatTableDataSource<User>;
+  config = new Configuration();
+  users: User[];
+
   private usersService: UsersService;
-  constructor(private httpClient: HttpClient, private authService: AuthService) {
-    const config = new Configuration();
-    config.basePath = '/authApi/api';
-    config.accessToken = this.authService.getAccessToken();
-    
-    this.usersService = new UsersService(this.httpClient, '/authApi/api', config);
-    let users: User[];
-    this.usersService.listUsers().subscribe(data=>{
-      users = data;
-    });
-    this.dataSource = new MatTableDataSource(users);
+  constructor(private httpClient: HttpClient, private userService: UserService) {
   }
 
   ngOnInit() {
+    this.config.basePath = '/authApi/api';
+    this.userService.listUsers().subscribe(data => {
+      this.users = data;
+      this.dataSource = new MatTableDataSource(this.users);
+      console.log(this.users)
+    });
   }
 }
--- src/app/core/service/user.service.ts
+++ src/app/core/service/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Observable, observable } from 'rxjs';
+import { User } from '@amitkshirsagar13/user-auth-service';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private authService: AuthService) { }
+
+  users: User[];
+
+  headers: HttpHeaders = new HttpHeaders()
+  .append('Authorization', this.authService.getAccessToken());
+  options = {
+    headers: this.headers,
+  }
+  public listUsers():Observable<User[]> {
+    return this.httpClient.get<User[]>('/authApi/api/users', this.options);
+  }
 }
