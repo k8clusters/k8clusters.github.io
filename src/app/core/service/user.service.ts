62df6570d4eabe4ad030f67dca9c9be53be2483b
Kshirsagar, Amit <amit.kshirsagar@siemens.com>
2020-02-24 09:48:38 +0530

    fixed

--- src/app/core/service/user.service.ts
+++ src/app/core/service/user.service.ts
@@ -1,24 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable, observable } from 'rxjs';
-import { User } from '@amitkshirsagar13/user-auth-service';
+import { User, UsersService } from '@amitkshirsagar13/user-auth-service';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor(private httpClient: HttpClient, private authService: AuthService) { }
-
-  users: User[];
-
-  headers: HttpHeaders = new HttpHeaders()
-  .append('Authorization', this.authService.getAccessToken());
-  options = {
-    headers: this.headers,
+  private usersService: UsersService
+  constructor(private httpClient: HttpClient, private authService: AuthService) { 
+    this.usersService = new UsersService(httpClient, '/authApi/api/users')
   }
+
   public listUsers():Observable<User[]> {
-    return this.httpClient.get<User[]>('/authApi/api/users', this.options);
+    this.usersService.defaultHeaders.append('Authorization', this.authService.getAccessToken());
+    return this.usersService.listUsers();
   }
 }
